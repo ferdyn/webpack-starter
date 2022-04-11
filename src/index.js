325b865ce27ff7230abe4683806a0de40bc27c95
FERDYN FIGUEREO MOYA <ferdynfiguereomoya@iMac-de-FERDYN.technicolor.net>
2022-04-11 23:41:26 +0200

    Primer commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+
+import { saludar } from './js/componentes';
+
+import './style.css';
+
+const nombre = 'Ferdyn';
+
+saludar( nombre );
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const HtmlWebpack    = require('html-webpack-plugin');
+const MiniCssExtract = require("mini-css-extract-plugin");
+const CopyPlugin     = require("copy-webpack-plugin");
+
+module.exports = {
+
+    mode: "development",
+
+    output: { 
+        clean: true 
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                loader: 'html-loader',
+                options: {
+                    sources: false
+                }
+            },
+            {
+                test: /\.css$/,
+                exclude: /style.css$/,
+                use: [ 'style-loader', 'css-loader' ]
+            },
+            {
+                test: /style.css$/,
+                use: [ MiniCssExtract.loader, 'css-loader']
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                  {
+                    loader: 'file-loader',
+                  },
+                ],
+            },
+        ]
+
+    },
+
+    optimization: { },
+
+    plugins: [
+        new HtmlWebpack({
+            title: 'Mi Webpack App',
+            //filename: 'index.hml',
+            template: './src/index.html'
+        }),
+        new MiniCssExtract({
+            filename: '[name].css',
+            ignoreOrder: false
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: 'src/assets/', to: 'assets/'}
+            ]
+        })
+    ]
+
+
+}
